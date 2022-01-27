60eb1aa3cd17c60efa2ae967dec9aa7f18d0dbb9
mika <unitycoder@users.noreply.github.com>
2022-01-27 21:57:44 +0200

    test #BUILD

--- Build.cmd
+++ Build.cmd
@@ -4,7 +4,7 @@ if exist Debug rd /s /q Debug
 if exist Release rd /s /q Release
 if exist x64 rd /s /q x64
 
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe"
+"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release
 
 :exit
 popd
