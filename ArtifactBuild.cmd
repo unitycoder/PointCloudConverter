183208864f6d9722bd10da69646d63eef08c5745
mika <unitycoder@users.noreply.github.com>
2024-09-09 23:18:44 +0300

    Update ArtifactBuild.cmd

--- ArtifactBuild.cmd
+++ ArtifactBuild.cmd
@@ -1,6 +1,6 @@
 @echo off
 pushd "%~dp0"
-powershell Compress-7Zip "Bin\x64\Release\net8.0-windows10.0.22621.0" -ArchiveFileName "PointCloudConverterX64.zip" -Format Zip
+powershell Compress-7Zip "bin\x64\Release\net8.0-windows10.0.22621.0" -ArchiveFileName "PointCloudConverterX64.zip" -Format Zip
 :exit
 popd
 @echo on
