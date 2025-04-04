4374f526ae8ddc187014f1343704d9ab190bc59f
unitycoder <support@unitycoder.com>
2025-04-04 22:06:59 +0300

    add missing file, #BUILD

---
+++ Structs/Metadata/Job.cs
@@ -0,0 +1,20 @@
+﻿using System.Text.Json.Serialization;
+
+namespace PointCloudConverter.Structs.Metadata
+{
+    public class Job
+    {
+        public string ConverterVersion { get; set; }
+        public ImportSettings ImportSettings { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Elapsed { get; internal set; }
+    }
+
+    public class JobMetadata
+    {
+        [JsonPropertyOrder(0)]
+        public Job Job { get; set; }
+        public List<LasHeader> lasHeaders { get; set; } = new List<LasHeader>();
+    }
+}
