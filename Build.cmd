04ed453aefa36785189c212b28679b10b72eecc1
unitycoder <support@unitycoder.com>
2025-05-18 12:45:28 +0300

    update build bat for 2022

--- Build.cmd
+++ Build.cmd
@@ -4,7 +4,8 @@ if exist Debug rd /s /q Debug
 if exist Release rd /s /q Release
 if exist x64 rd /s /q x64
 
-"%programfiles(x86)%\Microsoft Visual Studio\2019\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release
+"%programfiles%\Microsoft Visual Studio\2022\Enterprise\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release
+REM "%programfiles%\Microsoft Visual Studio\2022\Community\MSBuild\Current\Bin\msbuild.exe" /p:Configuration=Release
 
 :exit
 popd
